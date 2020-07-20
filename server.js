e9550752f01b21977300f3483cbcdbc3c132fcd2
Tey García <tgarciamiranda@gmail.com>
2020-07-19 20:07:13 -0600

    Handle not found

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ require('./models/db')
 
 app.use(bodyParser.urlencoded( { extended: false } ))
 app.use(bodyParser.json())
+
 app.use('/', router)
+// Handle 404
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found' })
+})
 
 app.listen(config.PORT, () => {
   console.log(`Goals api listening on port ${config.PORT}`)

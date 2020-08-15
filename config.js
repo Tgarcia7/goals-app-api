f01365b0b25ef7aeea1c39b083297728dc1b5957
Tey García <tgarciamiranda@gmail.com>
2020-08-14 23:12:42 -0600

    DB_URI default, dev and prod

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'dev',
-  DB_URI: process.env.DB_URI || `mongodb+srv://goals-admin:wuUVMZ8KprDFRWIc@goals.knuyh.mongodb.net/goals-${process.env.NODE_ENV || 'dev'}?retryWrites=true&w=majority`,
+  DB_URI: `mongodb+srv://goals-admin:wuUVMZ8KprDFRWIc@goals.knuyh.mongodb.net/goals-${process.env.NODE_ENV || 'dev'}?retryWrites=true&w=majority`,
   SECRET_TOKEN: process.env.SECRET_TOKEN || '123'
 }

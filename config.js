34aebf9911eee4bf8cc9ae3a3536f4b5c67a8c34
Tey García <tgarciamiranda@gmail.com>
2020-08-16 12:22:09 -0600

    new secrets

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'dev',
   DB_URI: `mongodb+srv://goals-admin:wuUVMZ8KprDFRWIc@goals.knuyh.mongodb.net/goals-${process.env.NODE_ENV || 'dev'}?retryWrites=true&w=majority`,
-  SECRET_TOKEN: process.env.SECRET_TOKEN || '123'
+  SECRET_TOKEN: process.env.SECRET_TOKEN || '2{5K*%Y[g7+GHqgr'
 }

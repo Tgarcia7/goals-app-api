41d312889b78f7ade2ba23ede8a875d0c0b24ef3
Tey García <tgarciamiranda@gmail.com>
2020-09-07 22:54:59 -0600

    secrets as env vars

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 module.exports = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'dev',
-  DB_URI: `mongodb+srv://goals-admin:wuUVMZ8KprDFRWIc@goals.knuyh.mongodb.net/goals-${process.env.NODE_ENV || 'dev'}?retryWrites=true&w=majority`,
+  DB_URI: process.env.DB_URI,
   SECRET_TOKEN: process.env.SECRET_TOKEN || '2{5K*%Y[g7+GHqgr'
 }
